c0191c2ab069f7899fb7aae5b65f2975bb7e1e90
liuyajie <1604249316@qq.com>
2023-03-28 11:05:47 +0800

    rocketmq-all init

---
+++ rocketmq-all/remoting/src/test/java/org/snail/lyj/AppTest.java
@@ -0,0 +1,20 @@
+package org.snail.lyj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
